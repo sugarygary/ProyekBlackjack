a80f7221f9a2b5685f40b189ef10451c074a23bd
Gary <Gary@192.168.0.8>
2022-05-13 09:38:19 +0700

    Login dan Lain lain

---
+++ src/proyekpbonew/Card.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyekpbonew;
+
+/**
+ *
+ * @author Inghwe
+ */
+public class Card {
+
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+}
---
+++ src/proyekpbonew/ProyekPBONew.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyekpbonew;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Gary
+ */
+public class ProyekPBONew {
+
+    private static ArrayList<User> userList = new ArrayList<>();
+
+    public ProyekPBONew() {
+        MainFrame logframe = new MainFrame();
+        logframe.setVisible(true);
+        logframe.setLocationRelativeTo(null);
+        logframe.setTitle("Blekjek");
+        logframe.setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        new ProyekPBONew();
+    }
+
+    public static ArrayList<User> getUserList() {
+        return userList;
+    }
+
+}
